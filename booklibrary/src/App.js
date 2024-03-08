fe536495cd63a10d7625b15a18cfd5be8180220c
Dilini Shashikala Gnanarathne <shashikaladilini11@gmail.com>
2024-03-08 22:54:13 +0530

    users.js file created & first name display

--- booklibrary/src/App.js
+++ booklibrary/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { Users } from './users';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <input type="text" placeholder="Search" className="search" />
+  <ul className="list">
+    {Users.map((user)=>(
+    <li className="listItem">{user.first_name}</li>))}
+  </ul>
     </div>
   );
 }
