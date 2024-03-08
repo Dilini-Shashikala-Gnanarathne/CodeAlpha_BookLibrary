9df86e418597073dab865cf64b085a67fcc8fe1d
Dilini Shashikala Gnanarathne <shashikaladilini11@gmail.com>
2024-03-08 23:42:54 +0530

    Server

--- booklibrary/src/App.js
+++ booklibrary/src/App.js
@@ -2,28 +2,29 @@ import { useEffect, useState } from "react";
 import { Users } from "./users";
 import "./App.css";
 import Table from "./Table";
+import axios from "axios";
+
 
 function App() {
   const [query, setQuery] = useState("");
-  const keys = ["heading", "location", "img"];
-
-  const Search = (data) => {
-    return data.filter((item) =>
-      keys.some((key) => item[key].toLowerCase().includes(query))
-    );
-  };
+  const [data, setData] = useState([]);
 
   useEffect(() => {
+    const fetchData = async () => {
+      const res = await axios.get(`http://localhost:5000?q=${query}`);
+      setData(res.data);
+    };
+    if (query.length === 0 || query.length > 2) fetchData();
   }, [query]);
 
   return (
     <div className="app">
-      <input
-        className="search"
-        placeholder="Search..."
-        onChange={(e) => setQuery(e.target.value.toLowerCase())}
-      />
-      <Table data={Search(Users)} />
+        <input
+          className="search"
+          placeholder="Search..."
+          onChange={(e) => setQuery(e.target.value.toLowerCase())}
+        />
+      {<Table data={data} />}
     </div>
   );
 }
